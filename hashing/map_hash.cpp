6706fea6e761e82a793623c6f165e927a1fd8585
Yogesh Sheoran <yogeshsheoranhr@gmail.com>
2025-08-22 21:22:34 +0530

    added hash map program

---
+++ hashing/map_hash.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    map<int , int> mpp;
+    for(int i = 0 ; i<n; i++){
+        mpp[arr[i]]++;
+    }
+
+    int q;
+    cout<<"enter number of queries : ";
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+        cout<<number<<" appears "<<mpp[number]<<" times"<<endl;
+    }
+}
