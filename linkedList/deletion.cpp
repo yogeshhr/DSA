8b2f2e70936460b79c04b3e705d368c2af435d2a
Yogesh Sheoran <yogeshsheoranhr@gmail.com>
2025-10-02 02:11:35 +0530

    added new operation in deletion of linked list program

--- linkedList/deletion.cpp
+++ linkedList/deletion.cpp
@@ -28,6 +28,65 @@ Node* removeHead(Node* head){
     return head;
 }
 
+Node* removetail(Node* head){
+    if(head == NULL || head->next == NULL) return NULL;
+    Node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = NULL;
+    return head;
+}
+
+Node* removeK(Node* head , int k){
+    if(head == NULL) return NULL;
+    if(k == 1){
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    
+    int count = 0;
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp != NULL){
+        count++;
+        if(count == k){
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+Node* removeElement(Node* head , int element){
+    if(head == NULL) return NULL;
+    if(head->data == element){
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp != NULL){
+        
+        if(temp->data == element){
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
 int main(){
     Node* head = new Node(10);
     head->next = new Node(20);
@@ -37,7 +96,23 @@ int main(){
     cout<<"Original List : ";
     print(head);
 
+    head = removeElement(head , 20);
+    cout<<"List after deletion of particular element : ";
+    print(head);
+
+    /*
+    head = removeK(head , 3);
+    cout<<"List after deletion at Kth position : ";
+    print(head);
+
+    
     head = removeHead(head);
     cout<<"List after deletion of head : ";
-    print(head); 
+    print(head);
+    
+    head = removetail(head);
+    cout<<"List after deletion of tail : ";
+    print(head);
+    */
+    
 }
