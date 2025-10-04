1084a67c3222ea4507df987051875acb8e023411
Yogesh Sheoran <yogeshsheoranhr@gmail.com>
2025-10-04 06:27:55 +0530

    added deletion in doubly linked list program

---
+++ linkedList/deletion_DLL.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node* back;
+
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+
+    Node(int data1 , Node* next1 ,Node* back1){
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+};
+
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout << temp->data;
+        if(temp->next) cout<< " <-> "; // only print arrow if next exists
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+Node* convertArr2DLL(vector<int> arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        prev->next = temp;
+        temp->back = prev;
+        prev = temp;
+    }
+    return head;
+}
+
+Node* deleteHead(Node* head){
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+    Node* prev = head;
+    head = head->next;
+    head->back = nullptr;
+    prev->next = nullptr;
+    delete prev;
+    return head;
+}
+
+int main(){
+        vector<int> arr = {2, 5, 8, 7};
+        
+        Node* head = convertArr2DLL(arr);
+        cout<<"original list : ";
+        print(head);
+
+        head = deleteHead(head);
+        cout<<"list after deletion of head : ";
+        print(head);
+}
